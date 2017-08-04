3883b255af340bf769a6f61d07f189dbb65faf6f
Hayaibrahim <Haya.aldkaan@gmail.com>
2017-08-04 15:10:31 +0300

    radiobutton

--- app/src/main/java/com/example/android/voic/MainActivity.java
+++ app/src/main/java/com/example/android/voic/MainActivity.java
@@ -1,22 +1,48 @@
 package com.example.android.voic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-     private SeekBar seekBar;
+
+    String[] audio = {"music1", "music2", "music3"};
+    ArrayAdapter<String> adapter;
+    private SeekBar seekBar;
+    private ListView mList;
+    Button open ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mList = (ListView) findViewById(R.id.Audio);
 
-        final MediaPlayer voic = MediaPlayer.create(MainActivity.this,R.raw.so);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, audio);
+        mList.setAdapter(adapter);
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getBaseContext(), adapterView.getItemAtPosition(i) + "is selectsf", Toast.LENGTH_LONG).show();
 
-        Button first = (Button)findViewById(R.id.on);
+            }
+        });
+
+
+        final MediaPlayer voic = MediaPlayer.create(MainActivity.this, R.raw.so);
+
+        Button first = (Button) findViewById(R.id.on);
         first.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button second = (Button)findViewById(R.id.off);
+        Button second = (Button) findViewById(R.id.off);
         second.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        Button pause = (Button)findViewById(R.id.pause);
+        Button pause = (Button) findViewById(R.id.pause);
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,7 +69,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+    open = (Button) findViewById(R.id.youtube);
+        open.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/"));
+                startActivity(i);
+            }
+
+
+    });
+}
+
+
+
+
+
+
+
 }
 
 
